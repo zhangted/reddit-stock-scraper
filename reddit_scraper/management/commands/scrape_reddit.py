b976e2bce341eb17abffe849603e6207063a2cbf
zhangted <tedcbook@gmail.com>
2021-07-15 22:31:09 -0400

    Update scrape_reddit.py

--- reddit_scraper/management/commands/scrape_reddit.py
+++ reddit_scraper/management/commands/scrape_reddit.py
@@ -2,6 +2,9 @@ from django.core.management.base import BaseCommand, CommandError
 from reddit_scraper.scripts import get_reddit
 
 class Command(BaseCommand):
+    """Run this through the heroku/terminal as './manage.py scrape_reddit.py' 
+       Set the command to run every hour through a scheduler.
+    """
     help = 'scrapes subreddits for ticker mentions'
 
     def handle(self, *args, **options):
