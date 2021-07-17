e8544736df1fca5e4ba80ea1fdde2401a120fcaf
zhangted <tedcbook@gmail.com>
2021-07-16 21:12:24 -0400

    Update get_reddit.py
    Cleaned up length of some functions, made regular expressions logic/counting tickers  into its own functions

--- reddit_scraper/scripts/get_reddit.py
+++ reddit_scraper/scripts/get_reddit.py
@@ -25,86 +25,102 @@ except:
     print('issue connecting to reddit api')
 
 def start():
-    """Call this to start scraping subreddits."""
-    subreddits = models.Subreddit.objects.all()
-    tickers = models.Ticker.objects.all()
-    tickers_dict = {t.ticker for t in tickers}
-    for s in subreddits: #for every subreddit, scrape the posts and comments and store into database.
-        scraped_dict = {}
-        get_post_title_data(s, 100, headers, scraped_dict)
-        get_comment_data(s, 1000, headers, scraped_dict)
-        store_data(s, scraped_dict)
+    """Start scraping all subreddits in the db for their posts and comments."""
+    subreddits = get_subreddits()
+    for s in subreddits:
+        scraped_dict = get_post_title_data(s, 100, headers, {}) #scrape post/titles
+        scraped_dict = get_comment_data(s, 1000, headers, scraped_dict) #scrape comments
+        store_data(s, scraped_dict) #store data in db
+
+def get_subreddits():
+    """Return all Subreddit rows."""
+    return models.Subreddit.objects.all()
+
+def get_tickers():
+    """Return all Ticker rows."""
+    return models.Ticker.objects.all()
+
+def get_valid_tickers_dict():
+    """Return all valid Tickers in a dict"""
+    i = get_tickers()
+    return {j.ticker for j in i}
 
 def roundDown_Hour_UTC(utc_float):
     """Round unix time down to the nearest hour."""
     return (utc_float // 3600) * 3600
 
 def set_PostTitle_lastTimestamp(Subreddit, utc_float):
-    """Sets the last timestamp seen from a scraped post/title for a subreddit."""
+    """Set the latest seen timestamp for post/titles in a subreddit. """
     Subreddit.post_title_last_timestamp = utc_float
     Subreddit.save()
 
 def set_Comments_lastTimestamp(Subreddit, utc_float):
-    """Ssets the last timestamp seen from a scraped comment for a subreddit"""
+    """Set the latest seen tiestamp for comments in a subreddit."""
     Subreddit.comments_last_timestamp = utc_float
     Subreddit.save()
 
 def set_SubredditUpdateTime(Subreddit):
-    """Sets the timestamp for when a subreddit was scraped."""
+    """Set the last updated time of a subreddit."""
     Subreddit.last_updated = timezone.now()
     Subreddit.save()
+
+def substitute_invalid_characters(text):
+    """Substitute characters that are invalid with spaces and returns new string. Ex. !!STOCK? --> STOCK """
+    text = re.sub(r'[-()\"#_/@;:<>{+=~*|`.!?,]', '    ', text)
+    text = re.sub(r'\n', '    ', text)
+    return text
+
+def check_text_for_tickers(text, valid_tickers_dict, scraped_dict):
+    valid_text = substitute_invalid_characters(text)
+    seen = set()
+    for w in valid_text.split(" "):
+        w = w.strip("'")
+        #exclude lowercase strings, strings that are not tickers, or strings that have been counted already
+        if not w.isupper() or w not in valid_tickers_dict or w in seen: 
+            continue
+        if w not in scraped_dict: 
+            scraped_dict[w] = 1
+        else: 
+            scraped_dict[w] += 1
+        seen.add(w)#add cur string to seen dictionary to prevent counting them twice
+    return scraped_dict
 
 def get_post_title_data(SubredditObj, limit, headers, scraped_dict):
     """Goes through a subreddit and scrapes posts/titles for stock tickers."""
     base_url = 'https://oauth.reddit.com/r/'
-    subreddit = SubredditObj.subreddit
-    data = requests.get(base_url + subreddit + "/new" + "?limit=" + str(limit), headers=headers).json()
+    subreddit_string = SubredditObj.subreddit
+    data = requests.get(base_url + subreddit_string + "/new" + "?limit=" + str(limit), headers=headers).json()
 
-    if not SubredditObj.post_title_last_timestamp:
-        lastGET_utctime = roundDown_Hour_UTC(time.time())
-    else:
+    if not SubredditObj.post_title_last_timestamp: #if there is no timestamp for the latest seen post/title
+        lastGET_utctime = roundDown_Hour_UTC(time.time()) #then the stop trigger is a post to the nearest hour, rounded down
+    else: 
         lastGET_utctime = max(SubredditObj.post_title_last_timestamp, roundDown_Hour_UTC(time.time()))
-
-    TickerObj = models.Ticker.objects.all() #get all tickers
-    ticker_dict = {t.ticker for t in TickerObj} #create ticker dict
+        #otherwise, the stop trigger is the max(cur time rounded down to nearest hour, latest seen post/title)
+    valid_tickers_dict = get_valid_tickers_dict() #get all valid tickers
     max_utc = lastGET_utctime #max utc seen so far
-    set_SubredditUpdateTime(SubredditObj)
-    while True: #loop
+    set_SubredditUpdateTime(SubredditObj) #set the subreddit update time
+    while True:
         after = data['data']['after'] #next page
         for i in data['data']['children']: #for every post
-            post_data = i['data']
+            post_data = i['data'] 
             time_posted = post_data['created_utc'] 
             max_utc = max(max_utc, time_posted) #update max utc seen
-            if lastGET_utctime and time_posted <= lastGET_utctime:
+            if lastGET_utctime and time_posted <= lastGET_utctime: #post has been scraped
                 set_PostTitle_lastTimestamp(SubredditObj, max_utc)
-                return #exit if the current post has already been scraped
-            text_data = post_data['title'] + post_data['selftext']
-            text_data = re.sub(r'[-()\"#_/@;:<>{+=~*|`.!?,]', '    ', text_data)
-            text_data = re.sub(r'\n', '    ', text_data)
-            seen = set()
-            for w in text_data.split(" "):
-                w = w.strip("'")
-                if not w.isupper():
-                    continue #exclude lowercase strings
-                if w not in ticker_dict:
-                    continue #exclude invalid strings
-                if w in seen:
-                    continue #exclude string seen alrdy in current post
-                if w not in scraped_dict:
-                    scraped_dict[w] = 1
-                else: 
-                    scraped_dict[w] += 1
-                seen.add(w)#one instance of a ticker per post+title
-        if not after:
+                return scraped_dict
+            unclean_text = post_data['title'] + post_data['selftext']
+            scraped_dict = check_text_for_tickers(unclean_text, valid_tickers_dict, scraped_dict)
+        if not after: #if no more pages to visit, then end function
             set_PostTitle_lastTimestamp(SubredditObj, max_utc)
-            return
-        try:
+            return scraped_dict
+        try: #after pages still exist, so get next page
             data = requests.get(base_url + subreddit + "/" + sort + "?limit=" + limit + "?after=" + after, 
-                                headers=headers).json() #get the next page
+                                headers=headers).json()
         except:
             print("Issue getting next page for title/post")
             break
     set_PostTitle_lastTimestamp(SubredditObj, max_utc)
+    return scraped_dict
 
 def get_comment_data(SubredditObj, limit, headers, scraped_dict):
     """Goes through a subreddit and scrapes comments for stock tickers."""
@@ -112,15 +128,14 @@ def get_comment_data(SubredditObj, limit, headers, scraped_dict):
     subreddit = SubredditObj.subreddit
     data = requests.get(base_url + subreddit + "/comments/?limit=" + str(limit), headers=headers).json()
 
-    if not SubredditObj.comments_last_timestamp:
-        lastGET_utctime = roundDown_Hour_UTC(time.time())
-    else:
+    if not SubredditObj.comments_last_timestamp: #if there is no timestamp for the latest seen post/title
+        lastGET_utctime = roundDown_Hour_UTC(time.time()) #then the stop trigger is a post to the nearest hour, rounded down
+    else: 
         lastGET_utctime = max(SubredditObj.comments_last_timestamp, roundDown_Hour_UTC(time.time()))
-
-    TickerObj = models.Ticker.objects.all() #get all tickers
-    ticker_dict = {t.ticker for t in TickerObj} #create ticker dict
+        #otherwise, the stop trigger is the max(cur time rounded down to nearest hour, latest seen post/title)
+    valid_tickers_dict = get_valid_tickers_dict() #get all valid tickers
     max_utc = lastGET_utctime #max utc seen so far
-    set_SubredditUpdateTime(SubredditObj)
+    set_SubredditUpdateTime(SubredditObj) #set the subreddit update time
     while True: #loop
         after = data['data']['after'] #next page
         for i in data['data']['children']: #for every post
@@ -129,41 +144,26 @@ def get_comment_data(SubredditObj, limit, headers, scraped_dict):
             max_utc = max(max_utc, time_posted) #update max utc seen
             if lastGET_utctime and time_posted <= lastGET_utctime:
                 set_Comments_lastTimestamp(SubredditObj, max_utc)
-                return #exit if the current post has already been scraped
-            text_data = post_data['body']
-            text_data = re.sub(r'[-()\"#_/@;:<>{+=~*|`.!?,]', '    ', text_data)
-            text_data = re.sub(r'\n', '    ', text_data)
-            seen = set()
-            for w in text_data.split(" "):
-                w = w.strip("'")
-                if not w.isupper():
-                    continue #exclude lowercase strings
-                if w not in ticker_dict:
-                    continue #exclude invalid strings
-                if w in seen:
-                    continue #exclude string seen alrdy in current post
-                if w not in scraped_dict:
-                    scraped_dict[w] = 1
-                else: 
-                    scraped_dict[w] += 1
-                seen.add(w)#one instance of a ticker per post+title
-        if not after:
+                return scraped_dict
+            unclean_text = post_data['body']
+            scraped_dict = check_text_for_tickers(unclean_text, valid_tickers_dict, scraped_dict)
+        if not after: #no more pages left to scrape, end function
             set_Comments_lastTimestamp(SubredditObj, max_utc)
-            return
-        try:
+            return scraped_dict
+        try: #try to get next page
             data = requests.get(base_url + subreddit + "/comments/?after=" + after + "&limit=" + str(limit), 
-                                headers=headers).json() #get the next page
+                                headers=headers).json() 
         except:
             print('Issue getting next page for comments')
             break
     set_Comments_lastTimestamp(SubredditObj, max_utc)
+    return scraped_dict
 
 def store_data(SubredditObj, scraped_dict):
-    """Stores dictionary created by scraping reddit posts in FrequencyEntries database."""
-    print(scraped_dict)
+    """Stores dictionary created by scraping reddit posts in FrequencyEntries object."""
+    print(SubredditObj.subreddit, scraped_dict)
     for t in scraped_dict:
-        query = models.FrequencyEntries(subreddit=SubredditObj.subreddit, ticker=t, count=scraped_dict[t])
+        query = models.FrequencyEntriesObj(subreddit=SubredditObj, ticker=t, count=scraped_dict[t])
         query.save()
 
-
-#exec(open('reddit_scraper/scripts/get_post_title.py').read())
+#exec(open('reddit_scraper/scripts/get_reddit.py').read())
